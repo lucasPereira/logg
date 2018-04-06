5e4e8c2b999a0d87127431a713065b7f531f8f08
carvoeira <lea@carvoeira>
2018-04-06 16:17:03 -0300

    Mostrando somente o HEAD e o master nos logs

--- configurar.sh
+++ configurar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git config --global alias.logg "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %s' --graph --all --topo-order --date=short"
-git config --global alias.logf "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %s' --name-only --all --topo-orde --date=short"
-git config --global alias.logh "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %s' --graph --follow --all --topo-order --date=short--"
+git config --global alias.logg "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %s' --graph --topo-order --date=short HEAD master"
+git config --global alias.logf "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %s' --name-only --topo-orde --date=short HEAD master"
+git config --global alias.logh "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %s' --graph --follow --topo-order --date=short-- HEAD master"
