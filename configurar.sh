519cf6088ba325415430c14e579c5e53397807bb
descartes <descartes@lea>
2018-06-25 11:08:00 -0300

    Definindo nome, email e editor do Git

--- configurar.sh
+++ configurar.sh
@@ -3,3 +3,6 @@
 git config --global alias.logg "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %s' --graph --topo-order --date=short HEAD master"
 git config --global alias.logf "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %s' --name-only --topo-orde --date=short HEAD master"
 git config --global alias.logh "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %s' --graph --follow --topo-order --date=short-- HEAD master"
+git config --global core.editor "vim"
+git config --global user.name `hostname`
+git config --global user.email `hostname`@`whoami`
