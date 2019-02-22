9b6c5a4a8caf3b75159c589910d34016b66c2141
Lucas Pereira da Silva <lucas@dominiol.com.br>
2019-02-22 07:43:49 -0300

    Não utilização fixa do master e documentação

--- configurar.sh
+++ configurar.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-git config --global alias.logg "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %s' --graph --topo-order --date=short HEAD master"
-git config --global alias.logf "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %s' --name-only --topo-order --date=short HEAD master"
-git config --global alias.logh "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %s' --graph --follow --topo-order --date=short-- HEAD master"
-git config --global core.editor "vim"
-git config --global user.name `hostname`
-git config --global user.email `hostname`@`whoami`
+git config --global alias.logg "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %s' --graph --topo-order --date=short HEAD"
+git config --global alias.logf "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %s' --name-only --topo-order --date=short HEAD"
+git config --global alias.logh "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %s' --graph --follow --topo-order --date=short-- HEAD"
--- readme.md
+++ readme.md
@@ -12,6 +12,25 @@ Alias Git para exibit os commits de um repositório em formato de árvore.
 
 ## Uso
 
+Mostrar, em formato de grafo, logs dos commits a partir do `HEAD`.
+
 ```
 git logg
+git logg master
+git logg feature
+```
+
+Mostrar, em formato de grafo, logs de arquivos adicionados, removidos ou modificados dos commits a partir do `HEAD`.
+
+```
+git logf
+git logf master
+git logf feature
+```
+
+Mostra commits onde um arquivo ou pasta foi modificado.
+
+```
+git logh arquivo.txt
+git logh pasta
 ```
