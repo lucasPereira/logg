bbba2240a3264e63b9ae670b6ccad0d4a7cd11ba
Lucas Pereira da Silva <lucas@dominiol.com.br>
2019-10-22 15:15:37 -0300

    Trunca mensagem de commit

--- configurar.sh
+++ configurar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git config --global alias.logg "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %s' --graph --topo-order --date=short HEAD master"
-git config --global alias.logf "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %s' --name-only --topo-orde --date=short HEAD master"
-git config --global alias.logh "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %s' --graph --follow --topo-order --date=short-- HEAD master"
+git config --global alias.logg "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %<(50,trunc)%s' --graph --topo-order --date=short HEAD master"
+git config --global alias.logf "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %<(50,trunc)%s' --name-only --topo-orde --date=short HEAD master"
+git config --global alias.logh "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %<(50,trunc)%s' --graph --follow --topo-order --date=short-- HEAD master"
