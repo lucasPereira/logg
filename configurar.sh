d5e4ac960cb75d8ffc7ace4bbc3beb378c6eebc9
Lucas Pereira da Silva <lucas@dominiol.com.br>
2021-02-16 14:32:53 -0300

    Mostra todas as referências

--- configurar.sh
+++ configurar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git config --global alias.logg "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %<(50,trunc)%s' --graph --topo-order --date=short HEAD"
-git config --global alias.logf "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %<(50,trunc)%s' --name-only --topo-order --date=short HEAD"
-git config --global alias.logh "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %<(50,trunc)%s' --graph --follow --topo-order --date=short-- HEAD"
+git config --global alias.logg "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %<(50,trunc)%s' --graph --topo-order --date=short --all"
+git config --global alias.logf "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %<(50,trunc)%s' --name-only --topo-order --date=short --all"
+git config --global alias.logh "log --pretty=format:'%C(auto)%h %Cred%ad%Creset%C(auto) %Cblue%an%Creset%C(auto)%d %<(50,trunc)%s' --graph --follow --topo-order --date=short --all --"
